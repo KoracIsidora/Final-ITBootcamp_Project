7550d6f70eeb67675755db484685e778352a52a5
Isidora <korac.isidora@gmail.com>
2020-01-01 18:05:26 +0100

    Ispravljen logout

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = ({ logedIn, setUser }) => {
+
     if (logedIn) {
         return (
-                <header>
-                    <Link to='/'><button onClick={() => { setUser({}) }} className='regbutth'>Logout</button></Link>
-                </header>
+            <header>
+                <Link to='/'><button onClick={() => { setUser() }} className='regbutth'>Logout</button></Link>
+            </header>
         )
     }
 
     return (
-            <header >
-                <Link to='/login'><button className='regbutth'>Login</button></Link>
-                <Link to='/register'> <button className='regbutth'>Register</button></Link>
-            </header> 
+        <header >
+            <Link to='/login'><button className='regbutth'>Login</button></Link>
+            <Link to='/register'> <button className='regbutth'>Register</button></Link>
+        </header>
     )
 }
 
